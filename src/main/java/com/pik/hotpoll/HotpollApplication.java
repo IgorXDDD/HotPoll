b6ccb6cf0c4fd817acb4c61d8d5a1bfa03f5fd02
Demongo2009 <kuba.galat1@wp.pl>
2021-03-18 16:05:04 +0100

    initial spring

---
+++ src/main/java/com/pik/hotpoll/HotpollApplication.java
@@ -0,0 +1,21 @@
+package com.pik.hotpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HotpollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotpollApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/pik/hotpoll/HotpollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pik.hotpoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotpollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
