32192358b35898ea22f88627eac3b39941f30a26
BartlomiejOlber <300237@pw.edu.pl>
2021-03-23 17:12:44 +0100

    killing

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,2 +1,3 @@
-if pgrep hotpoll; then pkill -f hotpoll; fi
+#if pgrep hotpoll; then pkill -f hotpoll; fi
+pkill -f hotpoll
 JENKINS_NODE_COOKIE=dontKillMe nohup java -jar target/hotpoll-0.0.1-SNAPSHOT.jar  &
