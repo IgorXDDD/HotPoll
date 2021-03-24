88cd8ac77a2576e5d6dd2587463c7a7889c823b7
areckt <arkadiusz.dawid.99@gmail.com>
2021-03-24 21:55:55 +0100

    link backend and frontend

--- src/main/java/com/pik/hotpoll/HotpollApplication.java
+++ src/main/java/com/pik/hotpoll/HotpollApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +16,17 @@ public class HotpollApplication {
 		SpringApplication.run(HotpollApplication.class, args);
 	}
 
+	// @GetMapping("/")
+	// public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	// 	return String.format("Hello %s!", name);
+	// }
+
 	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public ModelAndView frontend(ModelMap model) {
+		model.addAttribute("attribute", "frontend");
+		return new ModelAndView("forward:/frontend/index.html", model);
 	}
+
+	// public ModelAndView redirectWithUsingForwardPrefix(ModelMap model) {
+    //     model.addAttribute("attribute", "forwardWithForwardPrefix");
 }
