3875353124d26263a838927a9e2b393d5fa5e55d
areckt <arkadiusz.dawid.99@gmail.com>
2021-03-25 13:16:11 +0100

    add fetchData function onClick enterAsGuest button in LoginOrCreateAccount component

--- frontend/src/components/LoginOrCreateAccount.js
+++ frontend/src/components/LoginOrCreateAccount.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { useState } from "react";
 
+const testUrl = "localhost:4444/test";
+
 const LoginOrCreateAccount = () => {
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
+  const [testData, setTestData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(testUrl);
+      const data = await response.json();
+      setTestData(data);
+      console.log(testData);
+    } catch (error) {
+      console.log("Error with fetching test data: " + error);
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log("clicked login button");
+  };
+
+  const handleEnterAsGuest = () => {
+    console.log("clicked enter as a guest button");
+    fetchData();
   };
 
   return (
@@ -33,7 +53,10 @@ const LoginOrCreateAccount = () => {
       <div className="create-acc-or-guest">
         <button className="register-btn light-btn">Register</button>
         <h2>or</h2>
-        <button className="enter-as-guest-btn light-btn">
+        <button
+          className="enter-as-guest-btn light-btn"
+          onClick={handleEnterAsGuest}
+        >
           Enter as a Guest
         </button>
       </div>
