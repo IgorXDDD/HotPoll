57349b3bc62d355081fe5803cc53d4c19e4c4c94
Demongo2009 <kuba.galat1@wp.pl>
2021-03-25 12:14:43 +0100

    added backend simple test

--- src/test/java/com/pik/hotpoll/HotpollApplicationTests.java
+++ src/test/java/com/pik/hotpoll/HotpollApplicationTests.java
@@ -3,11 +3,20 @@ package com.pik.hotpoll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class HotpollApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getTestParam(){
+		HotpollApplication hotpollApplication = new HotpollApplication();
+		String result = hotpollApplication.testString("OK");
+		assertEquals(result, "Testing OK!");
 	}
 
 }
