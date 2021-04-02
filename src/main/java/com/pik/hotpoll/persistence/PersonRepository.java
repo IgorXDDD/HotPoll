21dc05dd97399f928dcfca5eb858197c78aac372
BartlomiejOlber <300237@pw.edu.pl>
2021-04-02 17:36:04 +0200

    scm polling test

--- src/main/java/com/pik/hotpoll/persistence/PersonRepository.java
+++ src/main/java/com/pik/hotpoll/persistence/PersonRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(collectionResourceRel = "people_2", path = "people_2")
 public interface PersonRepository extends MongoRepository<Person, String> {
 
     List<Person> findByLastName(@Param("name") String name);
