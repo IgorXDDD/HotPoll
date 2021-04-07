0de26bac10e2be5b8daf1695f62104e3ee999204
BartlomiejOlber <300237@pw.edu.pl>
2021-04-07 13:40:35 +0200

    deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,3 +1,2 @@
-#if pgrep hotpoll; then pkill -f hotpoll; fi
-pkill -f hotpoll
-JENKINS_NODE_COOKIE=dontKillMe nohup java -jar $1  &
+if pgrep -f hotpoll; then pkill -f hotpoll; fi
+JENKINS_NODE_COOKIE=dontKillMe nohup java -jar ${1} &
