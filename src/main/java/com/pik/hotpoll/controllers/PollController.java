1f8ea2522d2150ce054e6145f75a0c2a76efccfd
Demongo2009 <kuba.galat1@wp.pl>
2021-04-16 11:44:42 +0200

    fix json

--- src/main/java/com/pik/hotpoll/controllers/PollController.java
+++ src/main/java/com/pik/hotpoll/controllers/PollController.java
@@ -10,29 +10,29 @@ import org.springframework.web.bind.annotation.*;
 public class PollController {
     @GetMapping("")
     public String getPolls() {
-        JSONObject jo = new JSONObject();
-        jo.put("id",2137);
-        jo.put("title","Pineapple and Pizza?");
-        jo.put("date","16.04.2021");
-        jo.put("author","Demongo");
-        jo.put("timesCompleted",38);
-        jo.put("tags",new JSONArray()
-                .appendElement("food")
-                .appendElement("pineapple")
-                .appendElement("pizza"));
-        jo.put("alreadyCompleted",false);
-        jo.put("questions",new JSONArray()
-                .appendElement(new JSONObject()
-                .appendField("qid",1)
-                .appendField("question", "Does pineapple belong on pizza?")
-                .appendField("type","radio")
-                .appendField("answers", new JSONArray()
+        JSONObject jo = new JSONObject()
+                .appendField("id",2137)
+                .appendField("title","Pineapple and Pizza?")
+                .appendField("date","16.04.2021")
+                .appendField("author","Demongo")
+                .appendField("timesCompleted",38)
+                .appendField("tags",new JSONArray()
+                        .appendElement("food")
+                        .appendElement("pineapple")
+                        .appendElement("pizza"))
+                .appendField("alreadyCompleted",false)
+                .appendField("questions",new JSONArray()
                         .appendElement(new JSONObject()
-                                .appendField("aid",1)
-                                .appendField("answer","Hell Yeah!"))
-                        .appendElement(new JSONObject()
-                                .appendField("aid",2)
-                                .appendField("answer", "Eww!"))))
+                        .appendField("qid",1)
+                        .appendField("question", "Does pineapple belong on pizza?")
+                        .appendField("type","radio")
+                        .appendField("answers", new JSONArray()
+                                .appendElement(new JSONObject()
+                                        .appendField("aid",1)
+                                        .appendField("answer","Hell Yeah!"))
+                                .appendElement(new JSONObject()
+                                        .appendField("aid",2)
+                                        .appendField("answer", "Eww!"))))
         );
         return jo.toString();
     }
