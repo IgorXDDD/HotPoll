4c73978d34850b11b1011daaa273de761485b211
Demongo2009 <kuba.galat1@wp.pl>
2021-04-16 11:37:32 +0200

    add better json

--- src/main/java/com/pik/hotpoll/controllers/PollController.java
+++ src/main/java/com/pik/hotpoll/controllers/PollController.java
@@ -1,5 +1,6 @@
 package com.pik.hotpoll.controllers;
 
+import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +11,29 @@ public class PollController {
     @GetMapping("")
     public String getPolls() {
         JSONObject jo = new JSONObject();
-        jo.put("name","Cool poll");
-        jo.put("description","Very fabulous test poll.");
+        jo.put("id",2137);
+        jo.put("title","Pineapple and Pizza?");
+        jo.put("date","16.04.2021");
+        jo.put("author","Demongo");
+        jo.put("timesCompleted",38);
+        jo.put("tags",new JSONArray()
+                .appendElement("food")
+                .appendElement("pineapple")
+                .appendElement("pizza"));
+        jo.put("alreadyCompleted",false);
+        jo.put("questions",new JSONArray()
+                .appendElement(new JSONObject()
+                .appendField("qid",1)
+                .appendField("question", "Does pineapple belong on pizza?")
+                .appendField("type","radio")
+                .appendField("answers", new JSONArray()
+                        .appendElement(new JSONObject()
+                                .appendField("aid",1)
+                                .appendField("answer","Hell Yeah!"))
+                        .appendElement(new JSONObject()
+                                .appendField("aid",2)
+                                .appendField("answer", "Eww!"))))
+        );
         return jo.toString();
     }
 
