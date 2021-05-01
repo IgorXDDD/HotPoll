c8bc40d50ad97d8026d83ffe5234be711b4da114
Demongo2009 <kuba.galat1@wp.pl>
2021-05-01 10:06:16 +0200

    fixed not launching app, removed all security

--- src/main/java/com/pik/hotpoll/config/SecurityConfig.java
+++ src/main/java/com/pik/hotpoll/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 
 package com.pik.hotpoll.config;
 
-import com.pik.hotpoll.services.SecretService;
-import io.jsonwebtoken.Jwts;
+
+//import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +10,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -42,72 +45,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Value("${spring.h2.console.enabled:false}")
-    private boolean h2ConsoleEnabled;
 
-    @Bean
-    public JwtTokenFilter jwtTokenFilter() {
-        return new JwtTokenFilter();
-    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        if (h2ConsoleEnabled) {
-            http.authorizeRequests()
-                    .antMatchers("/h2-console", "/h2-console/**")
-                    .permitAll()
-                    .and()
-                    .headers()
-                    .frameOptions()
-                    .sameOrigin();
-        }
-
-        http.csrf()
-                .disable()
-                .cors()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS)
-                .permitAll()
-                .antMatchers("/about")
-                .permitAll()
-                .antMatchers(HttpMethod.GET, "/about")
-                .authenticated()
-                .antMatchers(HttpMethod.POST, "/poll", "/login")
-                .permitAll()
-                .antMatchers(HttpMethod.GET, "/test")
-                .permitAll()
-                .anyRequest()
-                .authenticated().and().oauth2ResourceServer().jwt();
-
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the
-        // wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(false);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
 
