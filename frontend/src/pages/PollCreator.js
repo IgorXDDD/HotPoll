85e562eb90ef7a27a9da9e18e765b0cdee2bec5c
areckt <arkadiusz.dawid.99@gmail.com>
2021-06-02 16:19:26 +0200

    tiny style improvements

--- frontend/src/pages/PollCreator.js
+++ frontend/src/pages/PollCreator.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import { useHistory,Link, Redirect } from 'react-router-dom'
-import SingleQuestion from '../components/pollsUtilities/SingleQuestionCreator'
-import { useGlobalContext } from '../context'
-import Tag from '../components/pollsUtilities/Tags'
-import AuthService from '../services/user.service.js'
-const API_URL = 'http://localhost:4444/api/poll/'
+import React, { useState } from 'react';
+import { useHistory, Link, Redirect } from 'react-router-dom';
+import SingleQuestion from '../components/pollsUtilities/SingleQuestionCreator';
+import { useGlobalContext } from '../context';
+import Tag from '../components/pollsUtilities/Tags';
+import AuthService from '../services/user.service.js';
+const API_URL = 'http://localhost:4444/api/poll/';
 
 const questionTemplate = {
   id: '',
@@ -22,15 +22,13 @@ const questionTemplate = {
       votes: 0,
     },
   ],
-}
-
-
+};
 
 function PollCreator() {
-  const [pollName, setPollName] = useState('')
+  const [pollName, setPollName] = useState('');
 
   let { logged, questions, tags, setQuestions, isGoogleLogged, googleInfo } =
-    useGlobalContext()
+    useGlobalContext();
   // const makeTen = () => {
   //   let ten = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
   //   let jwt = AuthService.getCurrentUser().jwt
@@ -70,8 +68,7 @@ function PollCreator() {
     // console.log('TAKA JEST ANKIETA')
     // console.log(questions)
 
-    if (isGoogleLogged) 
-    {
+    if (isGoogleLogged) {
       fetch(API_URL, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
@@ -98,11 +95,9 @@ function PollCreator() {
           questions: questions,
           tags: tags,
         }), // body data type must match "Content-Type" header
-      })
-    } 
-    else 
-    {
-      let jwt = AuthService.getCurrentUser().jwt
+      });
+    } else {
+      let jwt = AuthService.getCurrentUser().jwt;
       // Default options are marked with *
       fetch(API_URL, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -130,13 +125,12 @@ function PollCreator() {
           tags: tags,
           timesFilled: 0,
         }), // body data type must match "Content-Type" header
-      })
+      });
     }
     // console.log("PRZENOSIMY NA STRONE GLOWNA");
-    // console.log("TAKA LOKALIZACJA: "); 
+    // console.log("TAKA LOKALIZACJA: ");
     // console.log(window.location);
 
-    
     window.location.assign('/#/page/0');
     // window.location.href='/#'
     // window.location.reload();
@@ -144,19 +138,19 @@ function PollCreator() {
   }
   if (!logged) {
     return (
-      <div>
+      <div className="info">
         <h1>Please Sign in!</h1>
       </div>
-    )
+    );
   }
   return (
-    <div className='poll-wrapper'>
+    <div className="poll-wrapper">
       {/* <button onClick={makeTen}>dodaj 11</button> */}
       <h3>Poll name</h3>
       <input
-        type='text'
+        type="text"
         required
-        placeholder='enter poll name'
+        placeholder="enter poll name"
         value={pollName}
         onChange={(e) => setPollName(e.target.value)}
       />
@@ -164,15 +158,15 @@ function PollCreator() {
       {/* tutaj  wyswietlanie kazdego z pytan w ankiecie - jak na razie zostawilem domyslnie
              2 pytania */}
       {questions.map((question, index) => {
-        return <SingleQuestion questionIndex={index} key={index} />
+        return <SingleQuestion questionIndex={index} key={index} />;
       })}
 
       <button
         onClick={() => {
-          let newQuestion = {}
-          Object.assign(newQuestion, questionTemplate)
-          newQuestion.id = questions.length.toString()
-          setQuestions([...questions, newQuestion])
+          let newQuestion = {};
+          Object.assign(newQuestion, questionTemplate);
+          newQuestion.id = questions.length.toString();
+          setQuestions([...questions, newQuestion]);
         }}
       >
         Add new Question
@@ -183,16 +177,18 @@ function PollCreator() {
       <br />
       <h2>Submit Poll</h2>
       <form
-        action=''
+        action=""
         onSubmit={(e) => {
           // e.preventDefault()
           // console.log(questions)
           postData();
-          window.setTimeout(()=>{window.location.assign('/#/page/0')}, 50); 
+          window.setTimeout(() => {
+            window.location.assign('/#/page/0');
+          }, 50);
           // window.location.assign('/#/page/0')
         }}
       >
-        <button className='submit-btn'>Submit new poll</button>
+        <button className="submit-btn">Submit new poll</button>
       </form>
 
       {/* <button
@@ -205,7 +201,7 @@ function PollCreator() {
 
       {/* Przycisk do prostego debugowania tutaj wstawilem */}
     </div>
-  )
+  );
 }
 
-export default PollCreator
+export default PollCreator;
--- frontend/src/pages/SearchForm.js
+++ frontend/src/pages/SearchForm.js
@@ -1,52 +1,49 @@
-import React, {useState} from 'react'
-import {Search} from '../components/Search'
-import { useGlobalContext } from '../context'  
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Search } from '../components/Search';
+import { useGlobalContext } from '../context';
+import { Link } from 'react-router-dom';
 
 export default function SearchForm() {
-  const [searchTerm, setSearchTerm] = useState('')
-  const searchValue = React.useRef('')
-  const {logged} = useGlobalContext();
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchValue = React.useRef('');
+  const { logged } = useGlobalContext();
 
   React.useEffect(() => {
-    if(searchValue.current)
-    {
-      searchValue.current.focus()
+    if (searchValue.current) {
+      searchValue.current.focus();
     }
-  }, [])
+  }, []);
 
-  function searchPoll() 
-  {
-    setSearchTerm(searchValue.current.value)
+  function searchPoll() {
+    setSearchTerm(searchValue.current.value);
   }
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
   }
 
   return !logged ? (
-    <div>
+    <div className="info">
       <h2>Plase sign in first!</h2>
-      <Link to ='/'>
-        <button>
-          Go to home page
-        </button>
+      <Link to="/" className="link-underline">
+        <button>Go to home page</button>
       </Link>
     </div>
   ) : (
-    <section className='section search'>
-      <form className='search-form' onSubmit={handleSubmit}>
-        <div className='form-control'>
-          <label htmlFor='name'>search poll name</label>
-          <input
-            type='text'
-            name='name'
-            id='name'
-            ref={searchValue}
-            onChange={searchPoll}
-          />
-        </div>
+    <section>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <label className="search-form-label" htmlFor="name">
+          search poll name
+        </label>
+        <input
+          className="search-form-input"
+          type="text"
+          name="name"
+          id="name"
+          ref={searchValue}
+          onChange={searchPoll}
+        />
       </form>
       <Search searchTerm={searchTerm.toString()} />
     </section>
-  )
+  );
 }
